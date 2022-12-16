9b776c064a097764e9f202754c2b4422f94f671e
Ekksia X <beholder@ekksia.com>
2022-12-16 08:15:34 -0500

    Successfully added two customized items

--- src/main/java/net/ekksia/testmod/items/ModItemGroup.java
+++ src/main/java/net/ekksia/testmod/items/ModItemGroup.java
@@ -4,8 +4,12 @@ import net.ekksia.testmod.TestMod;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
-    public static final ItemGroup TANZANITE = FabricItemGroup.builder(new Identifier(TestMod.MOD_ID, "tanzanite")).build();
+    public static final ItemGroup TANZANITE = FabricItemGroup.builder(new Identifier(TestMod.MOD_ID))
+            .displayName(Text.literal("Tanzanite"))
+            .icon(() -> new ItemStack(ModItems.TANZANITE))
+            .build();
 }
